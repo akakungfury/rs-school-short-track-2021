b88a8761fc7ad7876b0be269204120b3b306ffd7
Aliaksandr Khudnitski <alekskhudnitskiy@gmail.com>
2021-04-11 22:37:32 +0300

    feat: add solution for mine-sweeper task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,62 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+
+function minesweeper(matrix) {
+  const columnCount = matrix[0].length;
+  const result = Array(matrix.length)
+    .fill()
+    .map(() => Array(columnCount).fill(0));
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < columnCount; j++) {
+      let counterOfMineAround = 0;
+
+      // if not first row
+      if (i !== 0) {
+        if (j > 0 && j < columnCount - 1) {
+          if (matrix[i - 1][j - 1]) counterOfMineAround++;
+          if (matrix[i - 1][j]) counterOfMineAround++;
+          if (matrix[i - 1][j + 1]) counterOfMineAround++;
+        } else if (j === 0) {
+          if (matrix[i - 1][j]) counterOfMineAround++;
+          if (matrix[i - 1][j + 1]) counterOfMineAround++;
+        } else if (j === columnCount - 1) {
+          if (matrix[i - 1][j - 1]) counterOfMineAround++;
+          if (matrix[i - 1][j]) counterOfMineAround++;
+        }
+      }
+
+      // if not last row
+      if (i !== matrix.length - 1) {
+        if (j > 0 && j < columnCount - 1) {
+          if (matrix[i + 1][j - 1]) counterOfMineAround++;
+          if (matrix[i + 1][j]) counterOfMineAround++;
+          if (matrix[i + 1][j + 1]) counterOfMineAround++;
+        } else if (j === 0) {
+          if (matrix[i + 1][j]) counterOfMineAround++;
+          if (matrix[i + 1][j + 1]) counterOfMineAround++;
+        } else if (j === columnCount - 1) {
+          if (matrix[i + 1][j - 1]) counterOfMineAround++;
+          if (matrix[i + 1][j]) counterOfMineAround++;
+        }
+      }
+
+      // common case
+      if (j > 0 && j < columnCount - 1) {
+        if (matrix[i][j - 1]) counterOfMineAround++;
+        if (matrix[i][j + 1]) counterOfMineAround++;
+      } else if (j === 0) {
+        if (matrix[i][j + 1]) counterOfMineAround++;
+      } else if (j === columnCount - 1) {
+        if (matrix[i][j - 1]) counterOfMineAround++;
+      }
+
+      result[i][j] = counterOfMineAround;
+    }
+  }
+
+  return result;
 }
 
 module.exports = minesweeper;
