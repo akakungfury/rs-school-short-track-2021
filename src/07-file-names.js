634ecd4d5b0fa682bc0e8097c13daba2fd7e4da0
Aliaksandr Khudnitski <alekskhudnitskiy@gmail.com>
2021-04-13 00:58:07 +0300

    feat: add solution for fole-names task

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,21 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+
+function renameFiles(names) {
+  names.forEach((currName, i, currArr) => {
+    const eqNames = currArr.filter((equalName, j) => currName === equalName && i !== j);
+
+    let eqNameNumber = 1;
+    eqNames.forEach(() => {
+      const equalNameIndex = currArr.findIndex((equalName, k) => currName === equalName && i !== k);
+      // eslint-disable-next-line no-param-reassign
+      names[equalNameIndex] = `${currName}(${eqNameNumber})`;
+      eqNameNumber++;
+    });
+  });
+
+  return names;
 }
 
 module.exports = renameFiles;
