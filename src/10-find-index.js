f2fb8beb344296b884244aa3638ec0a7e9e86785
Aliaksandr Khudnitski <alekskhudnitskiy@gmail.com>
2021-04-12 12:51:27 +0300

    feat: add solution for find-index task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,68 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+
+function findIndex(array, value) {
+  let start = 0;
+  let end = array.length;
+  let mid;
+  let index = 0;
+
+  while (start <= end) {
+    mid = Math.floor((end + start) / 2);
+
+    if (array[index + mid] === value) {
+      index = mid;
+      break;
+    }
+    if (array[mid] > value) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return index;
 }
+
+// solution using "for"
+// function findIndex(array, value) {
+//   let indexOfMiddleArrElem = Math.trunc(array.length / 2);
+//   let indexOfValue = 0;
+
+//   for (let i = 0; i < array.length; i++) {
+//     indexOfMiddleArrElem = Math.trunc(array.length / 2);
+
+//     if (array[indexOfMiddleArrElem] === value) {
+//       indexOfValue += indexOfMiddleArrElem;
+//       return indexOfValue;
+//     }
+//     if (array[indexOfMiddleArrElem] > value) {
+//       array = array.splice(0, indexOfMiddleArrElem);
+//     } else {
+//       indexOfValue += indexOfMiddleArrElem;
+//       array = array.splice(indexOfMiddleArrElem);
+//     }
+
+//     i = 0;
+//   }
+
+//   return indexOfValue;
+// }
+
+// recursive solution
+// function findIndex(array, value) {
+//   const indexOfMiddleArrElem = Math.trunc(array.length / 2);
+
+//   if (array[indexOfMiddleArrElem] > value) {
+//     return findIndex(array.splice(0, indexOfMiddleArrElem), value);
+//   }
+
+//   if (array[indexOfMiddleArrElem] < value) {
+//     return indexOfMiddleArrElem + findIndex(array.splice(indexOfMiddleArrElem), value);
+//   }
+
+//   return indexOfMiddleArrElem;
+// }
 
 module.exports = findIndex;
